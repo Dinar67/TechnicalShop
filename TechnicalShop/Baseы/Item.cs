6c3b5aeadfcca1e34e62f82b828a4503bef583c0
212111 <212111@WINMAC-4>
2023-10-13 16:35:37 +0300

    Добавьте файлы проекта.

---
+++ TechnicalShop/Baseы/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TechnicalShop.Baseы
+{
+    public partial class Product
+    {
+        public string CostDiscount
+        {
+            get
+            {
+                if (Discount == 0)
+                    return Cost.ToString("N0") + " ₽";
+                else
+                    return (Cost - (Cost * (decimal)Discount / 100)).ToString("N0") + " ₽  ";
+            }
+        }
+        public Visibility CostVisibility
+        {
+            get
+            {
+                if (Discount == 0)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+        }
+
+
+    }
+}
